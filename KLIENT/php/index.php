d1bfc2b805ba1f4e9aadd9847e6e46553b5b1a7a
Mehmet-Kaan <taspunar10@hotmail.com>
2022-04-29 23:13:24 +0200

    Header and Map
    Added google maps script and div element for map

--- KLIENT/php/index.php
+++ KLIENT/php/index.php
@@ -6,8 +6,14 @@
     <div id="wrapper">
 
     </div>
+
+    <header class="hide">THE MALMÖ PROJECT</header>
+    <div class="hide" id="map">
+    
     <script src="../scripts/functions.js"></script>
     <script src="../scripts/logIn.js"></script>
     <script src="../scripts/spelplanen.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC94Qi9OPA7V43ooB5f0gqSv8lmYm730RE&callback=initMap"
+    async defer></script>
 </body>
 </html>
