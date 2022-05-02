bc0e1d23fb85c37ec3fae7c08120c62a3247feba
Mehmet-Kaan <taspunar10@hotmail.com>
2022-05-02 11:51:39 +0200

    Update index.php

--- KLIENT/php/index.php
+++ KLIENT/php/index.php
@@ -19,8 +19,11 @@
 
     <script src="../scripts/functions.js"></script>
     <script src="../scripts/logIn.js"></script>
+    <script src="../scripts/spelplanen.js"></script>
     <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC94Qi9OPA7V43ooB5f0gqSv8lmYm730RE&callback=initMap" async defer></script>
     <script src="../scripts/spelplanen.js"></script> -->
-    
+    <?php
+        require_once "/api-key.php";
+    ?>
 </body>
 </html>
