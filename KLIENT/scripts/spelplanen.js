a075f6230c9ddf840e35ae33f2f1e089354cb1c3
Mehmet-Kaan <taspunar10@hotmail.com>
2022-05-06 12:04:01 +0200

    update

--- KLIENT/scripts/spelplanen.js
+++ KLIENT/scripts/spelplanen.js
@@ -14,6 +14,23 @@ async function setUserVariable(){
   let users = await getUsers();
   console.log(users)
   user = users.find((u) => u.userID == getFromSession("user").userID);
+
+  function initStart() {
+    // Checks if the user already entered the map view, to not give them permission to go backwards. 
+    if (getFromSession("entered") != true) {
+      pageThree(pageInformation, buttons, user);
+      // Fade In
+      const phaseThree = document.querySelector("#thirdPhase");
+      phaseThree.style.animation = "fadeIn 3s";
+    } else {
+      loadingPage();
+      setTimeout(() => {
+        displayMap();
+      }, 2000)
+
+    }
+  }
+  initStart();
 }
 
 setUserVariable();
@@ -24,19 +41,7 @@ if (getFromSession("user") === null) {
   window.location.replace("http://localhost:8000/php/index.php");
 };
 
-// Checks if the user already entered the map view, to not give them permission to go backwards. 
-if (getFromSession("entered") != true) {
-  pageThree(pageInformation, buttons, user);
-  // Fade In
-  const phaseThree = document.querySelector("#thirdPhase");
-  phaseThree.style.animation = "fadeIn 3s";
-} else {
-  loadingPage();
-  setTimeout(() => {
-    displayMap();
-  }, 2000)
 
-}
 
 // PAge three, also sets the user to user.
 function pageThree(info, button, user) {
